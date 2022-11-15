29366dcebb7a1b02509e7219039d5d511cc64a4c
Yerim96 <80906567+Yerim96@users.noreply.github.com>
2022-11-15 09:14:33 +0900

    master- 실행1추가

--- src/App.java
+++ src/App.java
@@ -4,12 +4,8 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세유");
-		System.out.println("실행2");
-		System.out.println("실행3");
-		System.out.println("집에보내달라우.....실행4");
-		System.out.println("집에 가고싶다 실행5");
-		System.out.println("실행 6");
-		System.out.println("실행 7");
+		System.out.println("실행1");
+
 		
 	
 	}
