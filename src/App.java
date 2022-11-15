9f916bdbd27facf9434c15a8142c122d25daab0c
yerim96 <chzh9993@naver.com>
2022-11-15 10:58:00 +0900

    추가

--- src/App.java
+++ src/App.java
@@ -10,9 +10,11 @@ public class App {
 		System.out.println("집에보내달라우.....실행4");
 		System.out.println("집에 가고싶다 실행5");
 		System.out.println("실행 6");
-		System.out.println("실행 7");
-System.out.println("branch-실행1");
-System.out.println("실행 2");
+		System.out.println("실행 7");	
+		System.out.println("branch-실행1");
+		System.out.println("실행 2");
+		System.out.println("실행 4");
+
 		
 	
 	}
